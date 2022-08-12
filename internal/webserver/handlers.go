8740e22e22c9d25ababe9ce585b2d3a7da5e4e05
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-08-11 23:35:02 -0300

    feat: add parse value inside template

--- internal/webserver/handlers.go
+++ internal/webserver/handlers.go
@@ -11,9 +11,20 @@ var (
 	fs   = http.FileServer(http.Dir("./internal/webserver/templates/static"))
 )
 
+type PerimeterType struct {
+	ID           int
+	Title        string
+	Type         string
+	ProjectCount int
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
-	err := tmpl.ExecuteTemplate(w, "Index", nil)
+	err := tmpl.ExecuteTemplate(w, "Index", []PerimeterType{
+		PerimeterType{1, "Perimeter One", "Regular", 10},
+		PerimeterType{2, "Perimeter Two", "Regular", 20},
+		PerimeterType{3, "Perimeter Three", "Regular", 30},
+	})
 	if err != nil {
 		log.Printf("Error executing template: %v", err)
 		return
